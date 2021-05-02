148b0dc81fcc976b51098fc51c11f1c1c07e7998
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-02 17:48:59 +0200

    FIX Fix the SQL scripts (DDL and DML) so that you can insert rows with specific IDs.

--- TourOfHeroesBEData/MSSQL/TourOfHeroesDB/Tables/TH_HERO.table.sql
+++ TourOfHeroesBEData/MSSQL/TourOfHeroesDB/Tables/TH_HERO.table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE TH_HERO
 (
-    ID          INT                 IDENTITY(1,1)      NOT NULL,
+    ID          INT                 IDENTITY(30,1)     NOT NULL,
     NAME        NVARCHAR(MAX)                          NOT NULL,
 
     CONSTRAINT PK_TH_HERO  PRIMARY KEY ( ID )
