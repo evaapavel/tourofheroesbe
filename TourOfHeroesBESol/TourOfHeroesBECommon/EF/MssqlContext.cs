b9302e8b0adcad53b1e917702cf611daa3620f1e
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-02 22:20:40 +0200

    CHANGE Add appsettings.json and MssqlContext. Add Microsoft.Extensions.Configuration.* packages.

---
+++ TourOfHeroesBESol/TourOfHeroesBECommon/EF/MssqlContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+using TourOfHeroesBECommon.BusinessObjects;
+
+
+
+namespace TourOfHeroesBECommon.EF
+{
+
+
+
+    public class MssqlContext : DbContext
+    {
+
+
+
+        private readonly string _connectionString;
+
+
+
+        public DbSet<Hero> Heroes { get; set; }
+
+
+
+        public MssqlContext()
+        {
+            // Temporary solution.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true);
+            var config = builder.Build();
+            //_connectionString = ConfigurationManager;
+            _connectionString = config["ConnectionStrings:TourOfHeroesDBConnectionString"];
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+
+
+    }
+
+
+
+}
