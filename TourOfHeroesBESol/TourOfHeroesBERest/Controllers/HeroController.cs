867291af09091d1d56ba69ab8e2d97b96502f6b5
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-04 05:04:42 +0200

    CHANGE Add an ASP.NET Core API project (TourOfHeroesBERest) to the TourOfHeroesBESol solution.

---
+++ TourOfHeroesBESol/TourOfHeroesBERest/Controllers/HeroController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TourOfHeroesBECommon.BusinessObjects;
+using TourOfHeroesBECommon.Services;
+
+
+
+namespace TourOfHeroesBERest.Controllers
+{
+
+
+
+    //[Route("api/heroes")]
+    [ApiController]
+    [Route("/api/heroes")]
+    public class HeroController : ControllerBase
+    {
+
+
+
+        private readonly ILogger<HeroController> _logger;
+
+        private readonly HeroService _heroService;
+
+
+
+        public HeroController(ILogger<HeroController> logger)
+        {
+            _logger = logger;
+            // Temporary solution
+            _heroService = new HeroService();
+        }
+
+
+
+        [HttpGet]
+        public IEnumerable<Hero> Get()
+        {
+            IEnumerable<Hero> heroes = _heroService.GetListOfHeroes();
+            //return heroes.ToArray<Hero[]>();
+            return heroes.ToArray<Hero>();
+            //return heroes.ToArray();
+        }
+
+
+
+        //// REST API path: GET /api/heroes
+        //[HttpGet]
+        //public async Task<IActionResult> GetList()
+        //{
+        //    IEnumerable<Hero> heroes = _heroService.GetListOfHeroes();
+        //    // HTTP status code: 200 (OK)
+        //    return Ok(heroes);
+        //}
+
+
+
+    }
+
+
+
+}
---
+++ TourOfHeroesBESol/TourOfHeroesBERest/Startup.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourOfHeroesBERest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Fix the CORS issue.
+            // Chrome:
+            // Access to XMLHttpRequest at 'http://localhost:26952/api/heroes' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            // Firefox:
+            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:26952/api/heroes. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing)
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200");
+                    });
+            }
+            );
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TourOfHeroesBERest", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TourOfHeroesBERest v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // Fix the CORS issue.
+            // Chrome:
+            // Access to XMLHttpRequest at 'http://localhost:26952/api/heroes' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            // Firefox:
+            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:26952/api/heroes. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing)
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
